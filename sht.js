477c037957dfcc25e7e9ddabeb599fe81f6dc7d5
haylienguyen <41085273+haylienguyen@users.noreply.github.com>
2018-07-10 15:09:37 -0500

    Create sht.js for allaboutme GitHub Pages

---
+++ sht.js
@@ -0,0 +1,8 @@
+function switchImages(){
+
+  document.getElementById("Sprinkles").src="https://i.pinimg.com/736x/d3/0e/fb/d30efbc99e95d684cb3c38c0e9d493e1--marshmallow-cupcakes-chocolate-marshmallows.jpg";
+}
+
+// function makeBigger(){
+//   document.getElementById("snake").src="https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Super%20Snakes/Snakes-fangs.ngsversion.1413818001976.adapt.1900.1.jpg"width="50%";
+// }
